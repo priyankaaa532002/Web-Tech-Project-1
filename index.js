0f782d7a4dd32eee16e2079c69983ac257b963ff
Tanishq Tyagi <tanishq.tyagi2020@vitstudent.ac.in>
2021-12-06 12:13:23 +0530

    Navigation done

--- index.js
+++ index.js
@@ -5,7 +5,23 @@ app.use(express.static("public"));
 app.use(express.static("images"));
 
 app.get("/", function (req, res) {
+    res.sendFile(__dirname + "/login.html");
+});
+
+app.post("/index.html", function (req, res) {
     res.sendFile(__dirname + "/index.html");
+});
+
+app.get("/snacks.html", function (req, res) {
+    res.sendFile(__dirname + "/snacks.html");
+});
+
+app.get("/main_course.html", function (req, res) {
+    res.sendFile(__dirname + "/main_course.html");
+});
+
+app.get("/dessert.html", function (req, res) {
+    res.sendFile(__dirname + "/dessert.html");
 });
 
 var server = app.listen(9000, function () {
